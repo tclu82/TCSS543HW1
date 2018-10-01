8eaaefc06ccbdf5e9db6f82ac573af041bcb93d4
Zac <tclu82@uw.edu>
2018-09-30 20:55:29 -0700

    Update .gitignore

--- TCSS543HW1StableMatching.java
+++ TCSS543HW1StableMatching.java
@@ -2,43 +2,61 @@ import java.util.*;
 
 public class TCSS543HW1StableMatching {
 
+    // Student list and hosiptal list, both list must have same size to fit the stable matching
     private static final String[] STUDENT_LIST = {"Frank", "Dennis", "Mac", "Charlie"};
     private static final String[] HOSPITAL_LIST = {"hospital1", "hospital2", "hospital3", "hospital4"};
 
+    // Each student has its own priority of hospitals
     private static final String[] FRANK_LIST = {"hospital3", "hospital2", "hospital1", "hospital4"};
     private static final String[] DENNIS_LIST = {"hospital2", "hospital4", "hospital1", "hospital3"};
     private static final String[] MAC_LIST = {"hospital3", "hospital1", "hospital4", "hospital2"};
     private static final String[] CHARLIE_LIST = {"hospital1", "hospital2", "hospital3", "hospital4"};
 
+    // Each hospital has its own priority of students
     private static final String[] HOSPITAL1_LIST = {"Frank", "Mac", "Dennis", "Charlie"};
     private static final String[] HOSPITAL2_LIST = {"Mac", "Charlie", "Dennis", "Frank"};
     private static final String[] HOSPITAL3_LIST = {"Dennis", "Mac", "Charlie", "Frank"};
     private static final String[] HOSPITAL4_LIST = {"Charlie", "Dennis", "Frank", "Mac"};
 
+    // Set of student
     private Set<String> studentSet;
+    // Set of hospital
     private Set<String> hospitalSet;
+    // Map of proposer and priority
     private Map<String, List<String>> preferenceMap;
-    private Map<String, Person> pairMap;
+    // Map of name and proposer
+    private Map<String, Proposer> pairMap;
 
+    /**
+     * Constructor to initialize all fields
+     */
     public TCSS543HW1StableMatching() {
         studentSet = new HashSet<>();
         hospitalSet = new HashSet<>();
         pairMap = new HashMap<>();
 
-        for (String man: STUDENT_LIST) {
-            studentSet.add(man);
-            pairMap.put(man, new Person(man));
+        for (String student: STUDENT_LIST) {
+            studentSet.add(student);
+            pairMap.put(student, new Proposer(student));
         }
-        for (String woman: HOSPITAL_LIST) {
-            hospitalSet.add(woman);
-            pairMap.put(woman, new Person(woman));
+        for (String hospital: HOSPITAL_LIST) {
+            hospitalSet.add(hospital);
+            pairMap.put(hospital, new Proposer(hospital));
         }
         preferenceMap = setPreferenceMap();
     }
 
+    /**
+     * Main method
+     *
+     * Choose matches.studentSet or matches.hospitalSet according to student's or hospital's preference
+     *
+     * @param theArgs
+     */
     public static void main(String... theArgs) {
         TCSS543HW1StableMatching matches = new TCSS543HW1StableMatching();
-        Set<Person> result = matches.propose(matches.hospitalSet);
+        Set<Proposer> result = matches.propose(matches.studentSet);
+//        Set<Proposer> result = matches.propose(matches.hospitalSet);
         System.out.println(result);
     }
 
@@ -81,11 +99,11 @@ public class TCSS543HW1StableMatching {
         return map;
     }
 
-    private Set<Person> propose(Set<String> proposerSet) {
+    private Set<Proposer> propose(Set<String> proposerSet) {
         if (studentSet.size() != hospitalSet.size()) {
             return null;
         }
-        Set<Person> result = new HashSet<>();
+        Set<Proposer> result = new HashSet<>();
         Queue<String> queue = new LinkedList<>();
         for (String proposer: proposerSet) {
             queue.add(proposer);
@@ -93,19 +111,19 @@ public class TCSS543HW1StableMatching {
 
         while (!queue.isEmpty()) {
             String proposer = queue.remove();
-            Person proposePerson = pairMap.get(proposer);
+            Proposer proposePerson = pairMap.get(proposer);
             List<String> targets = preferenceMap.get(proposer);
 
             String target = targets.get(0);
-            Person targetPerson = pairMap.get(target);
+            Proposer targetPerson = pairMap.get(target);
             // Not pair yet
-            if (targetPerson.partner == null) {
-                targetPerson.partner = proposer;
-                proposePerson.partner = target;
+            if (targetPerson.target == null) {
+                targetPerson.target = proposer;
+                proposePerson.target = target;
                 result.add(proposePerson);
                 // Check if unstable
             } else {
-                String enemy = targetPerson.partner;
+                String enemy = targetPerson.target;
                 List<String> targetPriorityList = preferenceMap.get(target);
                 int enemyPriority = targetPriorityList.indexOf(enemy);
                 int proposerPriority = targetPriorityList.indexOf(proposer);
@@ -124,11 +142,11 @@ public class TCSS543HW1StableMatching {
                     enemyPriorityList.remove(target);
                     targetPriorityList.remove(enemy);
 
-                    proposePerson.partner = target;
-                    targetPerson.partner = proposer;
+                    proposePerson.target = target;
+                    targetPerson.target = proposer;
                     result.add(proposePerson);
 
-                    Person enemyPerson = pairMap.get(enemy);
+                    Proposer enemyPerson = pairMap.get(enemy);
                     result.remove(enemyPerson);
                     queue.add(enemy);
                 }
@@ -138,16 +156,19 @@ public class TCSS543HW1StableMatching {
     }
 }
 
-class Person {
+/**
+ * Proposer class
+ */
+class Proposer {
     String name;
-    String partner;
+    String target;
 
-    Person(String name) {
+    Proposer(String name) {
         this.name = name;
     }
 
     @Override
     public String toString() {
-        return new String(name + ":" + partner);
+        return new String(name + ":" + target);
     }
 }
